6ce33217fc2dcfcbc5cefce4df50f1353b7b1877
Avik Pandey <avikpandey1@gmail.com>
2020-05-14 11:40:42 +0530

    ok

--- newProjectAdmin/server.js
+++ newProjectAdmin/server.js
@@ -20,12 +20,34 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',function(req,res){
-    console.log("ok");
+    
     res.render("index.ejs");
     // res.send("sucess");
 })
 
+app.get('/home',function(req,res){
+    res.render("home.ejs");
+})
 
-app.listen(3000, function() {
+app.get('/dailyLoggedIn',function(req,res){
+    res.render("dailyLoggedIn.ejs");
+})
+
+app.get('/DailySaleRecord',function(req,res){
+    res.render("DailySaleRecord.ejs");
+})
+
+app.post('/home',function(req,res){
+    console.log(req.body.email);
+    console.log(req.body.password);
+     if(req.body.email == 'avikmika@gmail.com' && req.body.password == 'iampandey'){
+         res.render('home.ejs');
+     }
+})
+
+
+
+
+app.listen(8080, function() {
     console.log("server started on");
   });
